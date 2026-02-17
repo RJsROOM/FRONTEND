bf665e2a1b8ba037d958f34dd65b9bc07effa944
Rakshit jha <rakshitjha05@gmail.com>
2026-02-17 17:22:13 +0530

    DOM start

---
+++ NOTES/DOM/script.js
@@ -0,0 +1,44 @@
+/* DOM: document object module (frontend ki javascript)
+DOM k 4 pillars-
+1. selection of an element
+    -querySelector
+    -getElementById
+    -getElementByClassName
+    -querySelectorAll
+2. changing HTML elements
+    - textContent
+    - innerHTML
+3. changing CSS properties
+    - style.[prop_name]
+4. event listeners.
+eg.-
+1. var h =document.querySelector('h1')
+console.log(h)
+
+2. var h1= document.querySelector('h1')
+h1.innerHTML= 'changed with JS'
+
+3. var h1= document.querySelector('h1')
+h1.innerHTML= 'changed with JS'
+h1.style.color='red'
+h1.style.backgroundColor='white'
+
+4. var h1= document.querySelector('h1')
+h1.innerHTML='changed'
+h1.style.color= 'gold'
+h1.addEventListener('click', function(){
+    console.log('clicked')
+})
+
+5. var h1= document.querySelector('h1')
+var btn= document.querySelector('button')
+
+btn.addEventListener('click', function(){
+    h1.innerHTML= "i am a Batman"
+    h1.style.color= "red"
+    h1.style.fontSize= '60px'
+})
+
+
+*/
+
