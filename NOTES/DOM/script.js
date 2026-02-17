f8188b417dbe731b65489b0e783576c123046fb3
Rakshit jha <rakshitjha05@gmail.com>
2026-02-17 20:22:22 +0530

    DOM-Day1

--- NOTES/DOM/script.js
+++ NOTES/DOM/script.js
@@ -39,6 +39,20 @@ btn.addEventListener('click', function(){
     h1.style.fontSize= '60px'
 })
 
+-----------------MINI PROPJECT ------------
+var box= document.getElementById('box')
+var btn=document.querySelector('button')
 
+btn.addEventListener('click', function(){
+    let a1= Math.floor(Math.random()*256)
+    let a2= Math.floor(Math.random()*256)
+    let a3= Math.floor(Math.random()*256)
+
+    box.style.backgroundColor= `rgb(${a1},${a2},${a3})`
+})
 */
+
+
+
+
 
