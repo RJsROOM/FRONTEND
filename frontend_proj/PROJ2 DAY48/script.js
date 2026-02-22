0b3250a72bd4bed31a855f356643b590816075c2
Rakshit jha <rakshitjha05@gmail.com>
2026-02-22 18:31:36 +0530

    proj2: piano with using DOM

---
+++ frontend_proj/PROJ2 DAY48/script.js
@@ -0,0 +1,234 @@
+var aud= [
+    new Audio("./audios/28.mp3"),
+    new Audio("./audios/29.mp3"),
+    new Audio("./audios/30.mp3"),
+    new Audio("./audios/31.mp3"),
+    new Audio("./audios/32.mp3"),
+    new Audio("./audios/33.mp3"),
+    new Audio("./audios/34.mp3"),
+    new Audio("./audios/35.mp3"),
+    new Audio("./audios/36.mp3"),
+    new Audio("./audios/37.mp3"),
+    new Audio("./audios/38.mp3"),
+    new Audio("./audios/39.mp3"),
+    new Audio("./audios/40.mp3"),
+    new Audio("./audios/41.mp3"),
+    new Audio("./audios/42.mp3"),
+    new Audio("./audios/43.mp3"),
+    new Audio("./audios/44.mp3"),
+    new Audio("./audios/45.mp3"),
+    new Audio("./audios/46.mp3"),
+    new Audio("./audios/47.mp3"),
+    new Audio("./audios/48.mp3"),
+    new Audio("./audios/49.mp3"),
+    new Audio("./audios/50.mp3"),
+    new Audio("./audios/51.mp3"),
+    new Audio("./audios/52.mp3"),
+    new Audio("./audios/53.mp3"),
+    new Audio("./audios/54.mp3"),
+    new Audio("./audios/55.mp3"),
+    new Audio("./audios/56.mp3"),
+    new Audio("./audios/57.mp3"),
+    new Audio("./audios/58.mp3"),
+    new Audio("./audios/59.mp3"),
+    new Audio("./audios/60.mp3"),
+    new Audio("./audios/61.mp3"),
+    new Audio("./audios/62.mp3"),
+    new Audio("./audios/63.mp3"),
+]
+
+var white1= document.getElementById('16')
+var white2= document.getElementById('17')
+var white3= document.getElementById('18')
+var white4= document.getElementById('19')
+var white5= document.getElementById('20')
+var white6= document.getElementById('21')
+var white7= document.getElementById('22')
+var white8= document.getElementById('23')
+var white9= document.getElementById('24')
+var white10= document.getElementById('25')
+var white11= document.getElementById('26')
+var white12= document.getElementById('27')
+var white13= document.getElementById('28')
+var white14= document.getElementById('29')
+var white15= document.getElementById('30')
+var white16= document.getElementById('31')
+var white17= document.getElementById('32')
+var white18= document.getElementById('33')
+var white19= document.getElementById('34')
+var white20= document.getElementById('35')
+var white21= document.getElementById('36')
+
+var black1= document.getElementById('1')
+var black2= document.getElementById('2')
+var black3= document.getElementById('3')
+var black4= document.getElementById('4')
+var black5= document.getElementById('5')
+var black6= document.getElementById('6')
+var black7= document.getElementById('7')
+var black8= document.getElementById('8')
+var black9= document.getElementById('9')
+var black10= document.getElementById('10')
+var black11= document.getElementById('11')
+var black12= document.getElementById('12')
+var black13= document.getElementById('13')
+var black14= document.getElementById('14')
+var black15= document.getElementById('15')
+
+
+
+white1.addEventListener('click', ()=>{
+    aud[0].currentTime=0;  //restart if clicked again
+    aud[0].play()
+})
+black1.addEventListener('click', ()=>{
+    aud[1].currentTime=0;  //restart if clicked again
+    aud[1].play()
+})
+white2.addEventListener('click', ()=>{
+    aud[2].currentTime=0;  //restart if clicked again
+    aud[2].play()
+})
+black2.addEventListener('click', ()=>{
+    aud[3].currentTime=0;  //restart if clicked again
+    aud[3].play()
+})
+white3.addEventListener('click', ()=>{
+    aud[4].currentTime=0;  //restart if clicked again
+    aud[4].play()
+})
+
+
+white4.addEventListener('click', ()=>{
+    aud[5].currentTime=0;  //restart if clicked again
+    aud[5].play()
+})
+black3.addEventListener('click', ()=>{
+    aud[6].currentTime=0;  //restart if clicked again
+    aud[6].play()
+})
+white5.addEventListener('click', ()=>{
+    aud[7].currentTime=0;  //restart if clicked again
+    aud[7].play()
+})
+black4.addEventListener('click', ()=>{
+    aud[8].currentTime=0;  //restart if clicked again
+    aud[8].play()
+})
+white6.addEventListener('click', ()=>{
+    aud[9].currentTime=0;  //restart if clicked again
+    aud[9].play()
+})
+black5.addEventListener('click', ()=>{
+    aud[10].currentTime=0;  //restart if clicked again
+    aud[10].play()
+})
+white7.addEventListener('click', ()=>{
+    aud[11].currentTime=0;  //restart if clicked again
+    aud[11].play()
+})
+
+
+white8.addEventListener('click', ()=>{
+    aud[12].currentTime=0;  //restart if clicked again
+    aud[12].play()
+})
+black6.addEventListener('click', ()=>{
+    aud[13].currentTime=0;  //restart if clicked again
+    aud[13].play()
+})
+white9.addEventListener('click', ()=>{
+    aud[14].currentTime=0;  //restart if clicked again
+    aud[14].play()
+})
+black7.addEventListener('click', ()=>{
+    aud[15].currentTime=0;  //restart if clicked again
+    aud[15].play()
+})
+white10.addEventListener('click', ()=>{
+    aud[16].currentTime=0;  //restart if clicked again
+    aud[16].play()
+})
+
+
+white11.addEventListener('click', ()=>{
+    aud[17].currentTime=0;  //restart if clicked again
+    aud[17].play()
+})
+black8.addEventListener('click', ()=>{
+    aud[18].currentTime=0;  //restart if clicked again
+    aud[18].play()
+})
+white12.addEventListener('click', ()=>{
+    aud[19].currentTime=0;  //restart if clicked again
+    aud[19].play()
+})
+black9.addEventListener('click', ()=>{
+    aud[20].currentTime=0;  //restart if clicked again
+    aud[20].play()
+})
+white13.addEventListener('click', ()=>{
+    aud[21].currentTime=0;  //restart if clicked again
+    aud[21].play()
+})
+black10.addEventListener('click', ()=>{
+    aud[22].currentTime=0;  //restart if clicked again
+    aud[22].play()
+})
+white14.addEventListener('click', ()=>{
+    aud[23].currentTime=0;  //restart if clicked again
+    aud[23].play()
+})
+
+
+white15.addEventListener('click', ()=>{
+    aud[24].currentTime=0;  //restart if clicked again
+    aud[24].play()
+})
+black11.addEventListener('click', ()=>{
+    aud[25].currentTime=0;  //restart if clicked again
+    aud[25].play()
+})
+white16.addEventListener('click', ()=>{
+    aud[26].currentTime=0;  //restart if clicked again
+    aud[26].play()
+})
+black12.addEventListener('click', ()=>{
+    aud[27].currentTime=0;  //restart if clicked again
+    aud[27].play()
+})
+white17.addEventListener('click', ()=>{
+    aud[28].currentTime=0;  //restart if clicked again
+    aud[28].play()
+})
+
+
+white18.addEventListener('click', ()=>{
+    aud[29].currentTime=0;  //restart if clicked again
+    aud[29].play()
+})
+black13.addEventListener('click', ()=>{
+    aud[30].currentTime=0;  //restart if clicked again
+    aud[30].play()
+})
+white19.addEventListener('click', ()=>{
+    aud[31].currentTime=0;  //restart if clicked again
+    aud[31].play()
+})
+black14.addEventListener('click', ()=>{
+    aud[32].currentTime=0;  //restart if clicked again
+    aud[32].play()
+})
+white20.addEventListener('click', ()=>{
+    aud[33].currentTime=0;  //restart if clicked again
+    aud[33].play()
+})
+black15.addEventListener('click', ()=>{
+    aud[34].currentTime=0;  //restart if clicked again
+    aud[34].play()
+})
+white21.addEventListener('click', ()=>{
+    aud[35].currentTime=0;  //restart if clicked again
+    aud[35].play()
+})
+
