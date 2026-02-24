179db60888b9d08568c3250449d568d7af18ac41
Rakshit jha <rakshitjha05@gmail.com>
2026-02-24 19:48:03 +0530

    proj: Spotlight effect and text animations

--- frontend_proj/PROJ4 DAY52-Spotlight effect/script.js
+++ frontend_proj/PROJ4 DAY52-Spotlight effect/script.js
@@ -8,15 +8,26 @@ const para= document.querySelector('p')
 const characters= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const text= para.innerText
 
+
+let interval=null;
+
 para.addEventListener('mouseenter', ()=>{
     //map() hme return me ek array deta h but forEach() kuch return ni krta islie map() use kiye h
-    setInterval(()=>{
+    let iteration=0;
+    clearInterval(interval);
+
+    function randomText(){
         const str= text.split('').map((char,index)=>{
+            if(index<iteration) return char;
+
             return characters.split('')[Math.floor(Math.random()*53)]
         }).join('')
         
         para.innerText= str;
 
-    }, 30)
+        iteration+=0.5;
+    }
+
+    interval=setInterval(randomText,30);
     
 })
