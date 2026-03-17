0d9c18670614dda52fcc3fd7309eabd07b388fea
Rakshit jha <rakshitjha05@gmail.com>
2026-03-17 20:08:15 +0530

    Back button mapping

--- frontend_proj/PROJ8 DAY66- Productivity Dashboard/script.js
+++ frontend_proj/PROJ8 DAY66- Productivity Dashboard/script.js
@@ -1,8 +1,15 @@
 var elems=document.querySelectorAll(".elem");
 var fullElems= document.querySelectorAll(".fullElem");
+var fullElemsBack= document.querySelectorAll(".fullElem .back")
 
 elems.forEach((elem)=>{
     elem.addEventListener("click", ()=>{
         fullElems[elem.id].style.display="block";
+    })
+})
+
+fullElemsBack.forEach((back)=>{
+    back.addEventListener("click", ()=>{
+        fullElems[back.id].style.display="none";
     })
 })
