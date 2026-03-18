5f0fb0a15206f23092a83a0906427456ec41805c
Rakshit jha <rakshitjha05@gmail.com>
2026-03-18 18:21:07 +0530

    logic building of Todo

--- frontend_proj/PROJ8 DAY66- Productivity Dashboard/script.js
+++ frontend_proj/PROJ8 DAY66- Productivity Dashboard/script.js
@@ -17,3 +17,63 @@ function openCrads(){
 }
 
 openCrads();
+
+let form= document.querySelector(".addTask form")
+
+let taskInput= document.querySelector(".addTask form input")
+let taskDetailsInput= document.querySelector(".addTask form textarea")
+let taskCheckbox= document.querySelector(".addTask form #check")
+
+let currentTask=[
+    {
+        task: "mandir jana h",
+        details: "sankat mochan",
+        checked: true
+    },
+    {
+        task: "badminton",
+        details: "kukkru-ankit ko call kro 4pm tk",
+        checked: true
+    },
+    {
+        task: "samaan lana h",
+        details: "tel or shampoo",
+        checked: false
+    },
+]
+
+function renderTask(){
+    var allTask= document.querySelector(".allTask");
+
+    var sum='';
+
+    currentTask.forEach((elem)=>{
+    sum+= `<div class="task">
+                <h5>${elem.task} 
+                    <span class=${elem.checked}>
+                        Imp
+                    </span>
+                </h5>
+                <button>Mark as complete</button>
+            </div>`
+    })
+
+    allTask.innerHTML=sum;
+}
+renderTask();
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    currentTask.push(
+        {   
+        task: taskInput.value, 
+        details: taskDetailsInput.value, 
+        checked: taskCheckbox.value
+        }
+    )
+    renderTask();
+    form.reset();
+})
+
+
