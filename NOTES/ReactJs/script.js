63323b2a60f85f185e60c2d82595fa87a39c6c01
Rakshit jha <rakshitjha05@gmail.com>
2026-03-21 00:08:00 +0530

    React basics

--- NOTES/ReactJs/script.js
+++ NOTES/ReactJs/script.js
@@ -1,1 +1,5 @@
-let h1= React.createElement('h1', null, 'hello world')
+let h1= React.createElement('h1', {id:'title', class:'attributes..'}, 'hello world')
+
+let container= document.querySelector('#container')
+
+ReactDOM.createRoot(container).render(h1)
