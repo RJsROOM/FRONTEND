3dd349c7bf51b9d40b853bbf31411f9589b32e4d
Rakshit jha <rakshitjha05@gmail.com>
2026-03-21 16:31:17 +0530

    React lect-01

---
+++ NOTES/ReactJs/class0/src/App.jsx
@@ -0,0 +1,5 @@
+const app= ()=>{
+  return <h1>hello</h1>
+}
+
+export default app;
--- NOTES/ReactJs/script.js
+++
@@ -1,5 +0,0 @@
-let h1= React.createElement('h1', {id:'title', class:'attributes..'}, 'hello world')
-
-let container= document.querySelector('#container')
-
-ReactDOM.createRoot(container).render(h1)
