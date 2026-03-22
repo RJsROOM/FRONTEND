07bf04519a1c29dc69c67de87f4551824e721fa0
Rakshit jha <rakshitjha05@gmail.com>
2026-03-22 17:17:57 +0530

    Day3: tailwindcss

---
+++ NOTES/ReactJs/class2/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="bg-gray-500 h-187 p-4">
+        <h2 className="text-5xl bg-amber-600 mt-2 font-bold rounded-xl p-5 ">RJsROOM</h2>
+    </div>
+  )
+}
+
+export default App
