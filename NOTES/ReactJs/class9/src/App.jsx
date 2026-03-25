04a2a2346c4a8abaa1421609e81ca25b1bbe5f36
Rakshit jha <rakshitjha05@gmail.com>
2026-03-25 19:59:59 +0530

    Day10: axios

--- NOTES/ReactJs/class9/src/App.jsx
+++ NOTES/ReactJs/class9/src/App.jsx
@@ -1,11 +1,16 @@
 import axios from 'axios'
+import { useState } from 'react'
+
 
 const App = () => {
+
+  const [allData, setAllData] = useState([])
 
   async function getData(){
     const response= await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
 
     console.log(response.data)
+    setAllData(response.data)
   }
   //axios is just a tool which helps in performing actions on the API and is much helpful in efficiency as well as scalability and SEO. since, getting results from the APIs can not get results in a fixed amt of time thus it gives responses in form of promises which can be handle through async-await.
   return (
@@ -15,6 +20,9 @@ const App = () => {
       onClick={getData}>
         Get data..
       </button>
+      {allData.map((elem,idx)=>{
+          return <h1 key={idx}>{elem.name} {idx+1}</h1>
+        })}
     </div>
   )
 }
