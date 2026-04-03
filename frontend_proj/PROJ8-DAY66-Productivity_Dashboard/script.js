7f2328f47162cd941e92866799304162b443ac23
Rakshit jha <rakshitjha05@gmail.com>
2026-04-03 16:57:20 +0530

    Making the weather widget

--- frontend_proj/PROJ8-DAY66-Productivity_Dashboard/script.js
+++ frontend_proj/PROJ8-DAY66-Productivity_Dashboard/script.js
@@ -1,3 +1,6 @@
+apiKey='5fd9759914674e2894c121738260204'
+city='varanasi'
+
 
 function openCrads() {
     var elems = document.querySelectorAll(".elem");
@@ -144,69 +147,85 @@ motivationalQuote();
 // ----------------------POMODORO TIMER------------------------------
 
 
-let timer= document.querySelector(".pomo-timer h1");
-let timerInterval=null;
-let startBtn= document.querySelector(".pomo-timer .start");
-let resetBtn= document.querySelector(".pomo-timer .reset");
-let pauseBtn= document.querySelector(".pomo-timer .pause");
-let session= document.querySelector(".pomo-fullpage .session");
-let isWorkSession=true;
+function pomodoroTimer() {
+    let timer = document.querySelector(".pomo-timer h1");
+    let timerInterval = null;
+    let startBtn = document.querySelector(".pomo-timer .start");
+    let resetBtn = document.querySelector(".pomo-timer .reset");
+    let pauseBtn = document.querySelector(".pomo-timer .pause");
+    let session = document.querySelector(".pomo-fullpage .session");
+    let isWorkSession = true;
 
-let totalSeconds= 25*60;
+    let totalSeconds = 25 * 60;
 
-function updateTimer(){
-    let minutes= Math.floor(totalSeconds/60);
-    let seconds=totalSeconds%60;
+    function updateTimer() {
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = totalSeconds % 60;
 
-    timer.innerHTML= `${minutes<10? '0'+minutes: minutes}:${seconds<10? '0'+seconds: seconds}`;
-}
-
-function startTimer(){
-    clearInterval(timerInterval);
-
-    if(isWorkSession){
-        timerInterval=setInterval(()=>{
-            if(totalSeconds>0){
-                totalSeconds--;
-                updateTimer();
-            }else{
-                isWorkSession=false;
-                clearInterval(timerInterval);
-                timer.innerHTML="05:00";
-                session.innerHTML="Break";
-                session.style.backgroundColor="var(--blue)";
-                totalSeconds=5*60;
-            }
-        },1000)
-    }else{
-        timerInterval=setInterval(()=>{
-            if(totalSeconds>0){
-                totalSeconds--;
-                updateTimer();
-            }else{
-                isWorkSession=true;
-                clearInterval(timerInterval);
-                timer.innerHTML="25:00";
-                session.innerHTML="Work Session";
-                session.style.backgroundColor="var(--green)";
-                totalSeconds=25*60;
-            }
-        },1000)
+        timer.innerHTML = `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
     }
+
+    function startTimer() {
+        clearInterval(timerInterval);
+
+        if (isWorkSession) {
+            timerInterval = setInterval(() => {
+                if (totalSeconds > 0) {
+                    totalSeconds--;
+                    updateTimer();
+                } else {
+                    isWorkSession = false;
+                    clearInterval(timerInterval);
+                    timer.innerHTML = "05:00";
+                    session.innerHTML = "Break";
+                    session.style.backgroundColor = "var(--blue)";
+                    totalSeconds = 5 * 60;
+                }
+            }, 1000)
+        } else {
+            timerInterval = setInterval(() => {
+                if (totalSeconds > 0) {
+                    totalSeconds--;
+                    updateTimer();
+                } else {
+                    isWorkSession = true;
+                    clearInterval(timerInterval);
+                    timer.innerHTML = "25:00";
+                    session.innerHTML = "Work Session";
+                    session.style.backgroundColor = "var(--green)";
+                    totalSeconds = 25 * 60;
+                }
+            }, 1000)
+        }
+    }
+
+    function pauseTimer() {
+        clearInterval(timerInterval);
+    }
+
+    function resetTimer() {
+        totalSeconds = 25 * 60;
+        clearInterval(timerInterval);
+        updateTimer();
+    }
+
+    startBtn.addEventListener("click", startTimer);
+    pauseBtn.addEventListener("click", pauseTimer);
+    resetBtn.addEventListener("click", resetTimer)
+
+    // the pause timer restarts from that last time because the value of totalseconds which already was deducted gets continued from that number only
 }
 
-function pauseTimer(){
-    clearInterval(timerInterval);
+pomodoroTimer();
+
+
+// ------------------------Weather Widget----------------------------
+
+async function weatherAPICall(){
+    let response=await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
+
+    let data= await response.json()
+    console.log(data)
 }
 
-function resetTimer(){
-    totalSeconds=25*60;
-    clearInterval(timerInterval);
-    updateTimer();
-}
-
-startBtn.addEventListener("click", startTimer);
-pauseBtn.addEventListener("click", pauseTimer);
-resetBtn.addEventListener("click", resetTimer)
-
-// the pause timer restarts from that last time because the value of totalseconds which already was deducted gets continued from that number only
+weatherAPICall();
