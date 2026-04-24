219a05ab6a18aa61a8509e50184516b2d2567943
Rakshit jha <rakshitjha05@gmail.com>
2026-04-24 21:23:10 +0530

    Add to Favourite feature is done..only UI updation is left

--- frontend_proj/PROJ10-DAY85-RECIPE_APPLICATION/Recipe/src/components/RecipeCard.jsx
+++ frontend_proj/PROJ10-DAY85-RECIPE_APPLICATION/Recipe/src/components/RecipeCard.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 
 const RecipeCard = (props) => {
 
-  const favourite= JSON.parse(localStorage.getItem('fav')) || [];
+  const [favourite, setFavourite] = useState(
+    JSON.parse(localStorage.getItem('fav')) || []
+  )
 
   const {id, image, title, description, chef, category}= props.recipe;
 
   const FavHandler=()=>{
-
+    let copyfav=[...favourite]
+    copyfav.push(props.recipe)
+    localStorage.setItem('fav', JSON.stringify(copyfav))
   }
 
   const UnFavHandler=()=>{
-
+    const filterfav= favourite.filter((f)=> f.id != props.recipe?.id)
+    setFavourite(filterfav)
+    localStorage.setItem('fav', JSON.stringify(filterfav))
   }
+
+
+  useEffect(()=>{
+  },[favourite])
+
+
 
   return (
     <Link 
@@ -21,7 +33,7 @@ const RecipeCard = (props) => {
     className='hover:scale-105 transition duration-250 block w-[20vw] mb-3 mr-3 rounded-xl overflow-hidden shadow-xl p-2'>
         <img className='object-cover w-full h-[25vh] rounded-lg mb-2  justify-center' src={image} alt="" />
         <div className='h-0.5 flex flex-row-reverse'>
-          {favourite.includes(props.recipe) ? (
+          {favourite.find((f)=> f.id == props.recipe?.id) ? (
             <i onClick={UnFavHandler} className="cursor-pointer text-2xl text-red-400 ri-poker-hearts-fill"></i>
           ) : (
             <i onClick={FavHandler} className="cursor-pointer text-2xl text-red-400 ri-poker-hearts-line"></i>
--- frontend_proj/PROJ10-DAY85-RECIPE_APPLICATION/Recipe/src/pages/Fav.jsx
+++ frontend_proj/PROJ10-DAY85-RECIPE_APPLICATION/Recipe/src/pages/Fav.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import RecipeCard from "../components/RecipeCard";
+
 
 const Fav = () => {
+  const favourite= JSON.parse(localStorage.getItem('fav') || [])
+
+  const renderRecipes= favourite.map((recipe)=>(
+    <RecipeCard key={recipe.id} recipe={recipe} />
+  ))
+
+
   return (
-    <div>Fav</div>
+    <div className="flex flex-wrap">
+      {favourite.length>0 ? renderRecipes : "No recipes found!"}
+    </div>
   )
 }
 
-export default Fav
+export default Fav;
